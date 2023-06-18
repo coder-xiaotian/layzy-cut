1fe1b57a535f371511152f7469cfcf9c958cbeb3
xiaotian <wanxiaotian888@gmail.com>
2023-06-18 21:20:37 +0800

    use custom server to start

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const http = require('http')
-const next = require('next')
+const http = require("http");
+const next = require("next");
 const app = next({
-  dir: './',
-  dev: true
-})
-const handle = app.getRequestHandler()
+  dir: "./",
+  dev: true,
+});
+const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
-    const port = 3000
+    const port = 3000;
     const server = http.createServer((req, res) => {
-      // 这一步非常有必要
-      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
 
-      handle(req, res)
-    })
+      handle(req, res);
+    });
 
-    server.listen(port)
+    server.listen(port);
   })
-  .catch(err => {
-    console.trace(err)
-  })
-
+  .catch((err) => {
+    console.trace(err);
+  });
