66f9e804494154c7163df5678f22f336cff63575
xiaotian <wanxiaotian888@gmail.com>
2023-06-18 22:25:49 +0800

    config headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [{
+      source: '/',
+      headers: [{
+        key: "Cross-Origin-Embedder-Policy",
+        value: "require-corp"
+      }, {
+        key: "Cross-Origin-Opener-Policy",
+        value: "same-origin"
+      }]
+    }]
+  }
+}
 
 module.exports = nextConfig
