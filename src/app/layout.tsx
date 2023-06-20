06dea7167690cfa9fa34457800930a52eb913eb1
xiaotian <wanxiaotian888@gmail.com>
2023-06-20 09:41:17 +0800

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Providers from './providers'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics/>
       </body>
     </html>
   )
