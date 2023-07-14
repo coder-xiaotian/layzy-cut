9ff3bf4383b91bc48d905a8c3223e8b9ed252d61
xiaotian <wanxiaotian888@gmail.com>
2023-07-14 12:34:15 +0800

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,34 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "懒人剪辑视频 - 快速剪辑工具",
-  description: "懒人剪辑视频是一个高效的在线工具，让你能够快速剪辑和编辑视频。节省时间，轻松实现你的视频编辑需求！",
+  applicationName: "LazyCut",
+  description:
+    "懒人剪辑视频是一个高效的在线工具，让你能够快速剪辑和编辑视频。节省时间，轻松实现你的视频编辑需求！",
+  viewport: {
+    initialScale: 1,
+    width: "device-width",
+  },
+  keywords: [
+    "ffmpeg",
+    "douyin",
+    "tiktok",
+    "video cut",
+    "next.js",
+    "react",
+    "剪辑",
+    "快速剪辑",
+    "LUT",
+    "auto LUT",
+    "批量 LUT",
+    "batch LUT",
+  ],
 };
 
 export default function RootLayout({
@@ -17,9 +38,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
         <Analytics />
