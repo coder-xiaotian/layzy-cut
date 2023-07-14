552eb07f181322c6b80b6a44c0f464ff89e55d5d
xiaotian <wanxiaotian888@gmail.com>
2023-07-14 12:35:12 +0800

    add analyze script

--- next.config.js
+++ next.config.js
@@ -1,27 +1,41 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
-    return [{
-      source: '/:path*',
-      headers: [{
-        key: "Cross-Origin-Embedder-Policy",
-        value: "require-corp"
-      }, {
-        key: "Cross-Origin-Opener-Policy",
-        value: "same-origin"
-      }]
-    }]
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
   },
+  // modularizeImports: {
+  //   "@mui/material": {
+  //     transform: "@mui/material/{{member}}",
+  //   },
+  // },
   experimental: {
     swcPlugins: [
       [
-        'use-client',
+        "use-client",
         {
-          include: ["@mui/material"]
-        }
+          include: ["@mui/material"],
+        },
       ],
-    ]
-  }
-}
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
